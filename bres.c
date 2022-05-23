4442107efc5229b3f16628b650c3063ffbc3cf9e
amaeldb <70023468+amaeldb@users.noreply.github.com>
2022-05-23 18:29:29 +0200

    Update bres.c

--- bres.c
+++ bres.c
@@ -60,13 +60,13 @@ void	bres_dr(t_fdf *fdf, t_vect start, t_vect end)
 	
 	delt.x = end.x - start.x;
 	delt.y = end.y - start.y;
-	pk = 2 * delt.y - delt.x;
+	pk = 2 * delt.y + delt.x;
 	while (start.x <= end.x && start.y >= end.y)
 	{
 		if (pk < 0)
 		{
 			place_pixel(start.x++, start.y--);
-			pk += 2 * delt.y - 2 * delt.x;
+			pk += 2 * delt.y + 2 * delt.x;
 		}
 		else
 		{
@@ -83,13 +83,13 @@ void	bres_dd(t_fdf *fdf, t_vect start, t_vect end)
 	
 	delt.x = end.x - start.x;
 	delt.y = end.y - start.y;
-	pk = 2 * delt.x - delt.y;
+	pk = 2 * delt.x + delt.y;
 	while (start.x <= end.x && start.y >= end.y)
 	{
 		if (pk < 0)
 		{
 			place_pixel(start.x++, start.y--);
-			pk += 2 * delt.x - 2 * delt.y;
+			pk += 2 * delt.x + 2 * delt.y;
 		}
 		else
 		{
